12391213d9e8db43a270ee981aef1134fd6b9ba0
mikedelcastillo <johnmichaeldc@gmail.com>
2020-09-05 11:24:32 +0800

    Added username to send-ip

--- send-ip.js
+++ send-ip.js
@@ -1,11 +1,15 @@
 const message = require('./message')
 const ip = require('ip').address()
+const os = require('os')
 
 let ports = ["3000", "5000", "8000", "8080"]
-let hostname = require('os').hostname()
+let username = os.userInfo().username
+let hostname = os.hostname()
+
 let lines = [
     "Machine: " + hostname,
+    ip,
     ...ports.map(port => `http://${ip}:${port}/`),
-    `ssh ${hostname}@${ip}`,
+    `ssh ${username}@${ip}`,
 ]
 message(lines.join("\n"))
